2156e1c2272c6efd7f48730dfa15e821f39c31db
gabrielbaute <gabrielbaute@gmail.com>
2026-01-21 16:38:57 -0400

    El logger se genera de acuerdo al nombre del modelo que se carga.

--- app/settings/logging_config.py
+++ app/settings/logging_config.py
@@ -1,16 +1,18 @@
 import logging
+from typing import Optional
 from app.settings.config import Config
 
-def setup_logging() -> None:
+def setup_logging(model_name: Optional[str] = None) -> None:
     """
     Función para establecer la configuración de registro básica.
     """
+    log_file_name: str = f"{Config.MODEL_NAME if not model_name else model_name}.log"
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] [%(name)s]: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
-            logging.FileHandler(str(Config.LOG_DIR / "app.log"), encoding="utf-8"),
+            logging.FileHandler(str(Config.LOG_DIR / log_file_name), encoding="utf-8"),
             logging.StreamHandler()
         ]
     )
